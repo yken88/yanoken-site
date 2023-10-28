aba0e01cd62714387c835331a31687eed8e3c62b
yken88 <yken1019@icloud.com>
2023-10-28 13:50:37 +0900

    🔥 remove imports script

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,11 @@
 import Navbar from "./Navbar";
-import Billing from "./Billing";
-import CardDeal from "./CardDeal";
-import Business from "./Business";
-import Clients from "./Clients";
-import CTA from "./CTA";
 import Skills from "./Skills";
 import Footer from "./Footer";
-import Testimonials from "./Testimonials";
 import Hero from "./Hero";
 
 export {
   Navbar,
-  Billing,
-  CardDeal,
-  Business,
-  Clients,
-  CTA,
   Skills,
   Footer,
-  Testimonials,
   Hero,
 };
